6e2279838526aa2fce45dddeb30d5075c0267d25
Michael Chen <michael.ld.chen@gmail.com>
2021-02-10 10:45:16 -0500

    Switch to Husky 4; try committing with linting error

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -4,7 +4,7 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
-export default function TabOneScreen() {
+export default function TabOneScreen () {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
