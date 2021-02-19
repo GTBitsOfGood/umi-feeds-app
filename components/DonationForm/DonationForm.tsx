a5b9447d7c0b51e9397d42d23ffd76902258e378
Charlie Luo <cvxluo@gmail.com>
2021-02-19 03:40:12 -0500

    Implements possible fix for datepicker opening unintentionally on Android

--- components/DonationForm/DonationForm.tsx
+++ components/DonationForm/DonationForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button } from 'react-native';
+import { Button, Platform } from 'react-native';
 
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { Input } from 'react-native-elements';
@@ -12,6 +12,9 @@ function DonationForm() {
   // Initially, the start datetime will be now, and the end will be a day from now
   const [endDatetime, setEndDatetime] = useState(new Date(Date.now() + 60 * 60 * 24 * 1000));
 
+  const [showStartDate, setShowStartDate] = useState(Platform.OS === 'ios');
+  const [showEndDate, setShowEndDate] = useState(Platform.OS === 'ios');
+
   const [description, setDescription] = useState('');
   const [pickupInstructions, setPickupInstructions] = useState('');
   const [weight, setWeight] = useState(0);
@@ -19,12 +22,16 @@ function DonationForm() {
   const onStartDatetimeChange = (event: any, selectedDatetime?: Date) => {
     const currentDatetime = selectedDatetime || startDatetime;
 
+    // Workaround for Android issue: https://github.com/react-native-datetimepicker/datetimepicker/issues/54
+    setShowStartDate(Platform.OS === 'ios');
+
     // Date.now() and currentDatetime.getTime() return milliseconds
     // We check if the date is within a valid range - in this case,
     // at least three hours in the future
-    if (Date.now() + 60 * 60 * 3 * 1000 > currentDatetime.getTime()) {
+    const cutoff = Date.now() + 60 * 60 * 3 * 1000;
+    if (cutoff > currentDatetime.getTime() || cutoff > endDatetime.getTime()) {
       setError('Date must be at least 3 hours from now!');
-    } else if (startDatetime > endDatetime) {
+    } else if (currentDatetime > endDatetime) {
       setError('Start availability must be before end availability!');
     } else setError('');
 
@@ -34,12 +41,16 @@ function DonationForm() {
   const onEndDatetimeChange = (event: any, selectedDatetime?: Date) => {
     const currentDatetime = selectedDatetime || endDatetime;
 
+    // Workaround for Android issue: https://github.com/react-native-datetimepicker/datetimepicker/issues/54
+    setShowEndDate(Platform.OS === 'ios');
+
     // Date.now() and currentDatetime.getTime() return milliseconds
     // We check if the date is within a valid range - in this case,
     // at least three hours in the future
-    if (Date.now() + 60 * 60 * 3 * 1000 > currentDatetime.getTime()) {
+    const cutoff = Date.now() + 60 * 60 * 3 * 1000;
+    if (cutoff > currentDatetime.getTime() || cutoff > startDatetime.getTime()) {
       setError('Date must be at least 3 hours from now!');
-    } else if (startDatetime > endDatetime) {
+    } else if (startDatetime > currentDatetime) {
       setError('Start availability must be before end availability!');
     } else setError('');
 
@@ -69,33 +80,43 @@ function DonationForm() {
     <View style={{ width: '100%' }}>
       <View style={{ width: '100%' }}>
         <Text>Availability Start</Text>
-        <DateTimePicker
-          value={startDatetime}
-          mode="date"
-          style={{ width: '100%' }}
-          onChange={onStartDatetimeChange}
-        />
-        <DateTimePicker
-          value={startDatetime}
-          mode="time"
-          style={{ width: '100%' }}
-          minuteInterval={15}
-          onChange={onStartDatetimeChange}
-        />
+        {showStartDate
+            && (
+            <View>
+              <DateTimePicker
+                value={startDatetime}
+                mode="date"
+                style={{ width: '100%' }}
+                onChange={onStartDatetimeChange}
+              />
+              <DateTimePicker
+                value={startDatetime}
+                mode="time"
+                style={{ width: '100%' }}
+                minuteInterval={15}
+                onChange={onStartDatetimeChange}
+              />
+            </View>
+            )}
         <Text>Availability End</Text>
-        <DateTimePicker
-          value={endDatetime}
-          mode="date"
-          style={{ width: '100%' }}
-          onChange={onEndDatetimeChange}
-        />
-        <DateTimePicker
-          value={endDatetime}
-          mode="time"
-          style={{ width: '100%' }}
-          minuteInterval={15}
-          onChange={onEndDatetimeChange}
-        />
+        {showEndDate
+            && (
+            <View>
+              <DateTimePicker
+                value={endDatetime}
+                mode="date"
+                style={{ width: '100%' }}
+                onChange={onEndDatetimeChange}
+              />
+              <DateTimePicker
+                value={endDatetime}
+                mode="time"
+                style={{ width: '100%' }}
+                minuteInterval={15}
+                onChange={onEndDatetimeChange}
+              />
+            </View>
+            )}
       </View>
       {/*
             Change to TextField at some point, or some other form of longer text input
