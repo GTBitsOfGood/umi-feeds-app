f5ab07a929360678f7e323ee7002fd34a224c7cd
whymarlene <marlenegarcia20@gmail.com>
2021-02-19 15:58:38 -0500

    added andriod package to config

--- app.config.ts
+++ app.config.ts
@@ -2,3 +2,9 @@ import dotenv from 'dotenv';
 
 // Load variables from .env file into environment variables
 dotenv.config({ path: '.env' });
+
+export default {
+  android: {
+    package: 'org.bitsofgood.umifeeds'
+  }
+};
