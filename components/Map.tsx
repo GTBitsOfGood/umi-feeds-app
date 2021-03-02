afb375201885e8cbc8311484a5a1e0e27655e014
Marlene Garcia <marlenegarcia20@gmail.com>
2021-03-02 00:03:17 -0500

    moved user loc to map and showed pin on map

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,19 +1,113 @@
-import * as React from 'react';
-import MapView from 'react-native-maps';
+import React, { useEffect, useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet, View, Dimensions } from 'react-native';
 
+import * as Location from 'expo-location';
+import { LocationObject } from 'expo-location';
+
+import { Text } from '../components/Themed';
+
 export default function Map() {
+  // get available pick up locations
+  // const [isLoading, setLoading] = useState<boolean>(true);
+  // const [availablePickup, setAvailablePickup] = useState([]);
+
+  // useEffect(() => {
+  //   fetch('http://localhost:3000/api/available-pickup')
+  //     .then((response) => response.json())
+  //     .then((data) => setAvailablePickup(data.donation))
+  //     .catch((error) => console.error(error))
+  //     .finally(() => setLoading(false));
+  // }, []);
+  // setAvailablePickup(tempGET);
+
+  // get user location
+  const [location, setLocation] = useState<LocationObject | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+      setLocation(await Location.getCurrentPositionAsync({}));
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = `My latitude: ${location.coords.latitude} \n My longitude: ${location.coords.longitude} \n\n JSON Junk: ${JSON.stringify(location)}`;
+  }
+
+  let userLocation;
+  if (location != null) {
+    userLocation = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude
+    };
+  } else {
+    userLocation = {
+      latitude: 33.7756,
+      longitude: -84.3963,
+    };
+  }
+
+  // const pickUps = tempGET.map((marker) => {
+  //   const dontationLocation = {
+  //     latitude: marker.donor.latitude,
+  //     longitude: marker.donor.longitude,
+  //   };
+  //   const key = marker._id;
+  //   const myMarker = (
+  //     <Marker
+  //       key={key}
+  //       coordinate={dontationLocation}
+  //       title={marker.donor.name}
+  //       description={marker.description}
+  //     />
+  //   );
+  //   return myMarker;
+  // });
+
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
         initialRegion={{
+          // latitude: userLocation.latitude,
+          // longitude: userLocation.longitude,
           latitude: 33.7490,
           longitude: -84.3880,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-      />
+      >
+        <Marker
+          coordinate={userLocation}
+        />
+        {tempGET.map((marker) => {
+          const dontationLocation = {
+            latitude: marker.donor.latitude,
+            longitude: marker.donor.longitude,
+          };
+          const key = marker._id;
+          const myMarker = (
+            <Marker
+              key={key}
+              coordinate={dontationLocation}
+              title={marker.donor.name}
+              description={marker.description}
+            />
+          );
+          return myMarker;
+        })}
+        <Text>{text}</Text>
+        <Text>{text}</Text>
+      </MapView>
     </View>
   );
 }
@@ -30,3 +124,66 @@ const styles = StyleSheet.create({
     height: Dimensions.get('window').height,
   },
 });
+
+const tempGET = [
+  {
+    descriptionImages: [],
+    foodImages: [],
+    _id: '6032bd3592899332cc7b25d4',
+    donor: {
+      _id: '602bf82713e73d625cc0d522',
+      name: 'Slutty Vegan',
+      latitude: 43.142,
+      longitude: -85.049
+    },
+    availability: {
+      _id: '6032bd3592899332cc7b25d5',
+      startTime: '2017-04-21T23:25:43.000Z',
+      endTime: '2022-04-21T23:25:43.000Z'
+    },
+    description: 'Plenty of delicious Impossible Burgers 2.0',
+    createdAt: '2021-02-21T20:06:13.164Z',
+    updatedAt: '2021-02-21T20:06:13.164Z',
+    __v: 0
+  },
+  {
+    descriptionImages: [],
+    foodImages: [],
+    _id: '603916d94808d4576cfad301',
+    donor: {
+      _id: '603916bb4808d4576cfad300',
+      name: 'BareBurger',
+      latitude: 43.142,
+      longitude: -85.049
+    },
+    availability: {
+      _id: '603916d94808d4576cfad302',
+      startTime: '2017-04-21T23:25:43.000Z',
+      endTime: '2022-04-21T23:25:43.000Z'
+    },
+    description: 'BareBurger Impossible Burgers 2.0',
+    createdAt: '2021-02-26T15:42:17.557Z',
+    updatedAt: '2021-02-26T15:42:17.557Z',
+    __v: 0
+  },
+  {
+    descriptionImages: [],
+    foodImages: [],
+    _id: '603917eb580586365c208958',
+    donor: {
+      _id: '603916bb4808d4576cfad300',
+      name: 'BareBurger',
+      latitude: 43.142,
+      longitude: -85.049
+    },
+    availability: {
+      _id: '603917eb580586365c208959',
+      startTime: '2017-04-21T23:25:43.000Z',
+      endTime: '2022-04-21T23:25:43.000Z'
+    },
+    description: 'BareBurger Impossible Burgers 2.0',
+    createdAt: '2021-02-26T15:46:51.993Z',
+    updatedAt: '2021-02-26T15:46:51.993Z',
+    __v: 0
+  }
+];
