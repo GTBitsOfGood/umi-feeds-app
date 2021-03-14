a6c4b460f4d8b01c3e78eba295613a4a59eebb66
Maria Shapiro <mariaashapiro@gmail.com>
2021-03-13 20:13:22 -0500

    added capability to take photos to the filepicker tab. works on android

--- screens/FilePickerScreen.tsx
+++ screens/FilePickerScreen.tsx
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Image, View, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import axios, { post } from 'axios';
+import axios from 'axios';
 
 export default function FilePickerScreen() {
   useEffect(() => {
     (async () => {
       if (Platform.OS !== 'web') {
-        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync() && await ImagePicker.requestCameraPermissionsAsync();
         if (status !== 'granted') {
           alert('Sorry, we need camera roll permissions to make this work!');
         }
       }
     })();
   }, []);
-  const [image, setImage] = useState<string | null>(null);
+
+  const [rollImage, setRollImage] = useState<string | null>(null);
+  const [takenImage, setTakeImage] = useState<string | null>(null);
+
   const pickImage = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
@@ -25,22 +28,49 @@ export default function FilePickerScreen() {
     let file;
     if (!result.cancelled) {
       file = { uri: result.uri, name: 'image.jpg' };
-      setImage(result.uri);
+      setRollImage(result.uri);
     }
-    const formData = new FormData();
-    formData.append('image', file as any);
+    const pickRollFormData = new FormData();
+    pickRollFormData.append('image', file as any);
     axios({
       method: 'post',
       url: 'http://localhost:3000/upload',
-      data: formData,
+      data: pickRollFormData,
       headers: { 'Content-Type': 'multipart/form-data' },
     });
   };
 
+  const takeImage = async () => {
+    const result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      quality: 1,
+    });
+    let file;
+    if (!result.cancelled) {
+      file = { uri: result.uri, name: 'image.jpg' };
+      setTakeImage(result.uri);
+    }
+    const takeCameraFormData = new FormData();
+    takeCameraFormData.append('image', file as any);
+    axios({
+      method: 'post',
+      url: 'http://localhost:3000/upload',
+      data: takeCameraFormData,
+      headers: { 'Content-Type': 'multipart/form-data' },
+    });
+  };
+
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Take a picture" onPress={takeImage} />
+      {takenImage && <Image source={{ uri: takenImage }} style={{ width: 200, height: 200 }} />}
+
+
       <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      {rollImage && <Image source={{ uri: rollImage }} style={{ width: 200, height: 200 }} />}
     </View>
+
   );
 }
