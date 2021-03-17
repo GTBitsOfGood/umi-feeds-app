b4011fb1fee9292fc6e8c4a7243d1a886abca51d
Michael Chen <michael.ld.chen@gmail.com>
2021-03-16 22:15:45 -0400

    Rename some variables

--- screens/FilePickerScreen.tsx
+++ screens/FilePickerScreen.tsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
-import { Button, Image, View, Platform } from 'react-native';
+import { Button, Image, View, Text, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import axios from 'axios';
 import { logAxiosError } from '../utils';
 
 export default function FilePickerScreen() {
-  const [rollImage, setRollImage] = useState<string | null>(null);
-  const [takenImage, setTakenImage] = useState<string | null>(null);
+  const [rollImage, setRollImage] = useState<string | null>(null); // uri of image chosen from user's photo library
+  const [cameraImage, setCameraImage] = useState<string | null>(null); // uri of image taken by camera
 
+  // Choose an image from the user's photo library and upload it to POST /upload
   const pickImage = async () => {
     if (Platform.OS !== 'web') {
       const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
       if (status !== 'granted') {
-        alert('Sorry, we need image roll permissions to be able to upload an image');
+        return;
       }
     }
     const result = await ImagePicker.launchImageLibraryAsync({
@@ -24,19 +25,19 @@ export default function FilePickerScreen() {
     if (!result.cancelled) {
       const file = { uri: result.uri, name: 'image.jpg', type: 'image/jpeg' };
       setRollImage(result.uri);
-      const pickRollFormData = new FormData();
-      pickRollFormData.append('image', file as any);
-      axios.post('/upload', pickRollFormData, { headers: { 'Content-Type': 'multipart/form-data' } })
+      const rollImageFormData = new FormData();
+      rollImageFormData.append('image', file as any);
+      axios.post('/upload', rollImageFormData, { headers: { 'Content-Type': 'multipart/form-data' } })
         .then((res) => console.log(res.data))
         .catch((err) => logAxiosError(err));
     }
   };
 
+  // Take a photo using the user's camera and upload it to POST /upload
   const takeImage = async () => {
     if (Platform.OS !== 'web') {
       const { status } = await ImagePicker.requestCameraPermissionsAsync();
       if (status !== 'granted') {
-        alert('Sorry, we need camera permissions to make this work!');
         return;
       }
     }
@@ -48,10 +49,10 @@ export default function FilePickerScreen() {
     });
     if (!result.cancelled) {
       const file = { uri: result.uri, name: 'image.jpg', type: 'image/jpeg' };
-      setTakenImage(result.uri);
-      const takeCameraFormData = new FormData();
-      takeCameraFormData.append('image', file as any);
-      axios.post('/upload', takeCameraFormData, { headers: { 'Content-Type': 'multipart/form-data' } })
+      setCameraImage(result.uri);
+      const cameraImageFormData = new FormData();
+      cameraImageFormData.append('image', file as any);
+      axios.post('/upload', cameraImageFormData, { headers: { 'Content-Type': 'multipart/form-data' } })
         .then((res) => console.log(res.data))
         .catch((err) => logAxiosError(err));
     }
@@ -60,8 +61,8 @@ export default function FilePickerScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Button title="Take a picture" onPress={takeImage} />
-      {takenImage && <Image source={{ uri: takenImage }} style={{ width: 200, height: 200 }} />}
-
+      {cameraImage && <Image source={{ uri: cameraImage }} style={{ width: 200, height: 200 }} />}
+      <Text>{'\n'}</Text>
       <Button title="Pick an image from camera roll" onPress={pickImage} />
       {rollImage && <Image source={{ uri: rollImage }} style={{ width: 200, height: 200 }} />}
     </View>
