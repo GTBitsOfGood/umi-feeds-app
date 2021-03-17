23259e222d3d1447623c90f1d58540d2824103ca
Michael Chen <michael.ld.chen@gmail.com>
2021-03-16 22:01:33 -0400

    Improve file picker
    Don't request permissions until the user clicks the button. Don't try to upload image if the user cancelled.

--- screens/FilePickerScreen.tsx
+++ screens/FilePickerScreen.tsx
@@ -1,64 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Button, Image, View, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import axios from 'axios';
 import { logAxiosError } from '../utils';
 
 export default function FilePickerScreen() {
-  useEffect(() => {
-    (async () => {
-      if (Platform.OS !== 'web') {
-        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync() && await ImagePicker.requestCameraPermissionsAsync();
-        if (status !== 'granted') {
-          alert('Sorry, we need camera roll permissions to make this work!');
-        }
-      }
-    })();
-  }, []);
-
   const [rollImage, setRollImage] = useState<string | null>(null);
-  const [takenImage, setTakeImage] = useState<string | null>(null);
+  const [takenImage, setTakenImage] = useState<string | null>(null);
 
   const pickImage = async () => {
+    if (Platform.OS !== 'web') {
+      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== 'granted') {
+        alert('Sorry, we need image roll permissions to be able to upload an image');
+      }
+    }
     const result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
       quality: 1,
       cropping: false,
     });
-    let file;
     if (!result.cancelled) {
-      file = { uri: result.uri, name: 'image.jpg', type: 'image/jpeg' };
+      const file = { uri: result.uri, name: 'image.jpg', type: 'image/jpeg' };
       setRollImage(result.uri);
+      const pickRollFormData = new FormData();
+      pickRollFormData.append('image', file as any);
+      axios.post('/upload', pickRollFormData, { headers: { 'Content-Type': 'multipart/form-data' } })
+        .then((res) => console.log(res.data))
+        .catch((err) => logAxiosError(err));
     }
-    const pickRollFormData = new FormData();
-    pickRollFormData.append('image', file as any);
-    axios.post('/upload', pickRollFormData, { headers: { 'Content-Type': 'multipart/form-data' } })
-      .then((res) => console.log(res.data))
-      .catch((err) => logAxiosError(err));
   };
 
   const takeImage = async () => {
+    if (Platform.OS !== 'web') {
+      const { status } = await ImagePicker.requestCameraPermissionsAsync();
+      if (status !== 'granted') {
+        alert('Sorry, we need camera permissions to make this work!');
+        return;
+      }
+    }
+
     const result = await ImagePicker.launchCameraAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
       quality: 1,
     });
-    let file;
     if (!result.cancelled) {
-      file = { uri: result.uri, name: 'image.jpg', type: 'image/jpeg' };
-      setTakeImage(result.uri);
+      const file = { uri: result.uri, name: 'image.jpg', type: 'image/jpeg' };
+      setTakenImage(result.uri);
+      const takeCameraFormData = new FormData();
+      takeCameraFormData.append('image', file as any);
+      axios.post('/upload', takeCameraFormData, { headers: { 'Content-Type': 'multipart/form-data' } })
+        .then((res) => console.log(res.data))
+        .catch((err) => logAxiosError(err));
     }
-    const takeCameraFormData = new FormData();
-    takeCameraFormData.append('image', file as any);
-    axios({
-      method: 'post',
-      url: '/upload',
-      data: takeCameraFormData,
-      headers: { 'Content-Type': 'multipart/form-data' },
-    })
-      .then((res) => console.log(res.data))
-      .catch((err) => handleAxiosError(err));
   };
 
   return (
