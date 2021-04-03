fe238023f3379596edfada88514bb2215ec0aba8
Colin Tam <colintam@Colins-MacBook-Pro.local>
2021-04-02 18:18:47 -0700

    Added some edit functions

--- components/DonationForm/DonationForm.tsx
+++ components/DonationForm/DonationForm.tsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 import HidableDatePicker from './HideableDatePicker';
 import { Text, View } from '../Themed';
 
-function DonationForm() {
+function DonationForm(props: {donationId?: string}) {
   const [description, setDescription] = useState('');
   const [pickupInstructions, setPickupInstructions] = useState('');
   const [weight, setWeight] = useState<number | ''>('');
@@ -27,6 +27,29 @@ function DonationForm() {
     });
   };
 
+  //Edit donation functions
+  const editSubmit = () => {
+    axios.put(`/api/donations/${props.donationId}`, {
+      donorID: '602bf82713e73d625cc0d522',
+      availability: {
+        startTime: startDatetime,
+        endTime: endDatetime,
+      },
+      description: description !== '' ? description : undefined,
+      pickupInstructions: pickupInstructions !== '' ? pickupInstructions : undefined,
+      weight: weight !== '' ? weight : undefined,
+    });
+  };
+
+  const deleteSubmit = () => {
+    axios.delete(`/api/donations/${props.donationId}`)
+  };
+
+
+  const donationInfo = axios.get(`/api/donations/${props.donationId}`);
+  //
+
+
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -41,43 +64,89 @@ function DonationForm() {
     },
   });
 
-  return (
-    <View style={{ width: '100%' }}>
-      <ScrollView style={styles.scrollView}>
-        <Text>Availability Start</Text>
-        <HidableDatePicker datetime={startDatetime} setDatetime={setStartDatetime} />
-        <Text>Availability End</Text>
-        <HidableDatePicker datetime={endDatetime} setDatetime={setEndDatetime} />
-        {(Date.now() + 60 * 60 * 2 * 1000) > endDatetime.getTime()
-          && <Text style={{ color: 'red' }}>End time is preferred to be at least two hours from now</Text>
-        }
-        {/*
-        Change to TextField at some point, or some other form of longer text input
-        Border styling is needed so the TextFields are visible on iOS
-        */}
-        <Input
-          label="Description"
-          value={description}
-          onChangeText={(desc: string) => setDescription(desc)}
-        />
-        <Input
-          label="Pickup Instructions (optional)"
-          value={pickupInstructions}
-          onChangeText={(instructions: string) => setPickupInstructions(instructions)}
-        />
-        <Input
-          label="Weight of Food (pounds, optional)"
-          value={weight.toString()}
-          onChangeText={(weight: string) => setWeight(Number.isFinite(+weight) && weight !== '' ? +weight : '')}
-          keyboardType="numeric"
-        />
-        <Button
-          title="Submit"
-          onPress={() => handleSubmit()}
-        />
-      </ScrollView>
-    </View>
-  );
+  if (props.donationId != null) {
+    return (
+      <View style={{ width: '100%' }}>
+        <ScrollView style={styles.scrollView}>
+          <Text>Availability Start</Text>
+          <HidableDatePicker datetime={startDatetime} setDatetime={setStartDatetime} />
+          <Text>Availability End</Text>
+          <HidableDatePicker datetime={endDatetime} setDatetime={setEndDatetime} />
+          {(Date.now() + 60 * 60 * 2 * 1000) > endDatetime.getTime()
+            && <Text style={{ color: 'red' }}>End time is preferred to be at least two hours from now</Text>
+          }
+          {/*
+          Change to TextField at some point, or some other form of longer text input
+          Border styling is needed so the TextFields are visible on iOS
+          */}
+          <Input
+            label="Description"
+            value={description}
+            onChangeText={(desc: string) => setDescription(desc)}
+          />
+          <Input
+            label="Pickup Instructions (optional)"
+            value={pickupInstructions}
+            onChangeText={(instructions: string) => setPickupInstructions(instructions)}
+          />
+          <Input
+            label="Weight of Food (pounds, optional)"
+            value={weight.toString()}
+            onChangeText={(weight: string) => setWeight(Number.isFinite(+weight) && weight !== '' ? +weight : '')}
+            keyboardType="numeric"
+          />
+          <Button
+            title="Submit"
+            onPress={() => editSubmit()}
+          />
+          <Button
+            title="Delete"
+            onPress={() => deleteSubmit()}
+          />
+        </ScrollView>
+      </View>
+    );
+  } else {
+    return (
+      <View style={{ width: '100%' }}>
+        <ScrollView style={styles.scrollView}>
+          <Text>Availability Start</Text>
+          <HidableDatePicker datetime={startDatetime} setDatetime={setStartDatetime} />
+          <Text>Availability End</Text>
+          <HidableDatePicker datetime={endDatetime} setDatetime={setEndDatetime} />
+          {(Date.now() + 60 * 60 * 2 * 1000) > endDatetime.getTime()
+            && <Text style={{ color: 'red' }}>End time is preferred to be at least two hours from now</Text>
+          }
+          {/*
+          Change to TextField at some point, or some other form of longer text input
+          Border styling is needed so the TextFields are visible on iOS
+          */}
+          <Input
+            label="Description"
+            value={description}
+            onChangeText={(desc: string) => setDescription(desc)}
+          />
+          <Input
+            label="Pickup Instructions (optional)"
+            value={pickupInstructions}
+            onChangeText={(instructions: string) => setPickupInstructions(instructions)}
+          />
+          <Input
+            label="Weight of Food (pounds, optional)"
+            value={weight.toString()}
+            onChangeText={(weight: string) => setWeight(Number.isFinite(+weight) && weight !== '' ? +weight : '')}
+            keyboardType="numeric"
+          />
+          <Button
+            title="Submit"
+            onPress={() => handleSubmit()}
+          />
+        </ScrollView>
+      </View>
+    );
+  }
+
+  
 }
 
 export default DonationForm;
