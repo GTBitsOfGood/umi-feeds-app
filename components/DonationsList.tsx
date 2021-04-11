7f73bf4fbce41084a91100252fcb9e93859f3061
Michael Ryan <michael_ryan_2000@yahoo.com>
2021-04-10 20:24:06 -0400

    Restyled donations list page (#74)
    Co-authored-by: Michael Chen <michael.ld.chen@gmail.com>

--- components/DonationsList.tsx
+++ components/DonationsList.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { ScrollView, RefreshControl, StyleSheet, View, Button, Dimensions } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-
+import { DateTime } from 'luxon';
 import { Text } from '../components/Themed';
 import { Donation } from '../types';
 import { store } from '../redux/store';
@@ -20,26 +20,34 @@ export default function DonationsList() {
       .finally(() => setLoading(false));
   }, []);
 
-  const wait = (timeout: number) => new Promise((resolve) => {
-    setTimeout(resolve, timeout);
-  });
-
   const onRefresh = React.useCallback(() => {
     setRefreshing(true);
     axios.get<{ donations: Donation[] }>('/api/donations', { headers: { Authorization: `Bearer ${store.getState().auth.jwt}` } })
-      .then((res) => setDonations(res.data.donations))
+      .then((res) => {
+        setDonations(res.data.donations);
+        setRefreshing(false);
+      })
       .catch((error) => logAxiosError(error))
       .finally(() => setLoading(false));
-    wait(2000).then(() => setRefreshing(false));
   }, []);
 
-  // need to fix refresh
-  const donationList = isLoading ? [] : donations.map((donation) => (
-    <DonationListBox
-      key={donation._id}
-      {...donation}
-    />
-  ));
+  const ongoingDonations: JSX.Element[] = [];
+  const pastDonations: JSX.Element[] = [];
+  if (!isLoading) {
+    donations.forEach((donation) => {
+      if (donation.pickup !== undefined && 'pickupTime' in donation.pickup) {
+        pastDonations.push(<DonationListBox
+          key={donation._id}
+          {...donation}
+        />);
+      } else {
+        ongoingDonations.push(<DonationListBox
+          key={donation._id}
+          {...donation}
+        />);
+      }
+    });
+  }
 
   return (
     <View style={styles.container}>
@@ -47,56 +55,81 @@ export default function DonationsList() {
         contentContainerStyle={styles.scrollView}
         refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
       >
-        <Text style={styles.title}>Donations List</Text>
-        <Text>Pull down to see RefreshControl indicator</Text>
-        <Button
-          title="refresh"
-          onPress={onRefresh}
-        />
-        {donationList}
+        <Text style={styles.title}>All Donations</Text>
+        <Text style={styles.subtitle}>Ongoing Donations</Text>
+        {ongoingDonations}
+        <Text style={styles.subtitle}>Completed Donations</Text>
+        {pastDonations}
       </ScrollView>
     </View>
   );
 }
 
-function DonationListBox(donation:Donation) {
+function DonationListBox(donation: Donation) {
   const navigation = useNavigation();
+  const endTime = DateTime.fromISO(donation.availability.endTime).toLocaleString(DateTime.TIME_SIMPLE);
+  let pickupTime = 'TBA';
+  let color = '#FC8834';
+  if (donation.pickup !== undefined && 'pickupTime' in donation.pickup) {
+    pickupTime = DateTime.fromISO(donation.pickup.pickupTime).toLocaleString(DateTime.TIME_SIMPLE);
+    color = '#3E3E3E';
+  }
   return (
-    <View style={styles.donationContainer}>
-      <Text
-        onPress={() => navigation.navigate('DetailDonation', {
-          donation
-        })
-      }
-      >
-        {donation.description}
-      </Text>
+    <View
+      style={{
+        flex: 1,
+        borderWidth: 1,
+        marginBottom: 10,
+        borderRadius: 20,
+        padding: 10,
+        borderColor: color,
+      }}
+    >
+      <View style={{ flex: 1, marginBottom: 10, flexDirection: 'row', justifyContent: 'space-between' }}>
+        <Text style={{ color, fontSize: 18, fontWeight: 'bold' }}>{endTime}</Text>
+        <Text
+          style={{ color: '#4B78CB', fontSize: 16, fontWeight: 'bold' }}
+          onPress={() => navigation.navigate('DetailDonation', {
+            donation
+          })}
+        >
+          View ⟩
+        </Text>
+      </View>
+      <View style={{ flex: 1, marginBottom: 10 }}>
+        <Text style={{ color, fontSize: 14 }}>{donation.description}</Text>
+      </View>
+      <View style={{ flex: 1 }}>
+        <Text style={{ color, fontSize: 14 }}>{`Picked up at: ${pickupTime}`}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   scrollView: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: '10%'
+    flexGrow: 1,
+    width: '80%',
+    margin: '10%',
+    paddingBottom: 35,
   },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: Dimensions.get('screen').width,
   },
   title: {
-    fontSize: 20,
+    fontSize: 48,
     fontWeight: 'bold',
+    color: '#FC8834',
+    paddingVertical: 10,
   },
-  donationContainer: {
-    flex: 1,
-    fontSize: 20,
-    width: Dimensions.get('window').width,
-    height: 20,
-    alignContent: 'space-around',
-    alignSelf: 'center',
+  subtitle: {
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 24,
+    lineHeight: 23,
+    color: '#3E3E3E',
+    marginBottom: 20,
+    marginTop: 10,
   }
 });
