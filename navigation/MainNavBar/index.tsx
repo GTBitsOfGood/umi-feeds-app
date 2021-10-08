c12aafc5933a38f862f67f54cafdcf15d75649b1
sydbalcom <sbalcom3@gatech.edu>
2021-10-08 13:03:01 -0400

    add drop shadow and new colors to navigation bar

--- navigation/MainNavBar/index.tsx
+++ navigation/MainNavBar/index.tsx
@@ -19,7 +19,7 @@ const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 // You can explore the built-in icon families and icons on the web at:
 // https://icons.expo.fyi/
 function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string }) {
-  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
+  return <Ionicons size={30} style={{ marginBottom: -3, paddingVertical: 10 }} {...props} />;
 }
 
 // Each tab has its own navigation stack, you can read more about this pattern here:
@@ -30,7 +30,19 @@ function DonorTabs() {
   return (
     <BottomTab.Navigator
       initialRouteName="Home"
-      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}
+      tabBarOptions={{
+        activeTintColor: Colors[colorScheme].tint,
+        style: { height: '8%',
+          borderTopLeftRadius: 4,
+          borderTopRightRadius: 4,
+          shadowColor: 'rgba(144, 144, 144, 1)',
+          shadowOpacity: 0.15,
+          shadowOffset: {
+            width: 0,
+            height: -4,
+          },
+          shadowRadius: 12
+        } }}
     >
       <BottomTab.Screen
         name="Home"
@@ -43,7 +55,7 @@ function DonorTabs() {
         name="Donate"
         component={DonationScreenNavigator}
         options={{
-          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="hand-heart" size={30} color={color} style={{ marginBottom: -3 }} />,
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="hand-heart" size={30} color={color} style={{ marginBottom: -3, paddingVertical: 10 }} />,
         }}
       />
       <BottomTab.Screen
@@ -63,7 +75,18 @@ function AdminTabs() {
   return (
     <BottomTab.Navigator
       initialRouteName="Home"
-      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}
+      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint,
+        style: { height: '8%',
+          borderTopLeftRadius: 4,
+          borderTopRightRadius: 4,
+          shadowColor: 'rgba(144, 144, 144, 1)',
+          shadowOpacity: 0.15,
+          shadowOffset: {
+            width: 0,
+            height: -4,
+          },
+          shadowRadius: 12
+        } }}
     >
       <BottomTab.Screen
         name="Home"
