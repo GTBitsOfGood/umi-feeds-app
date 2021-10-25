f9839784fe7d5194ffcc0aacc8432d1227e947a7
Ivan Zou <izou3@gatech.edu>
2021-10-25 15:00:00 -0400

    Dynamic ScrollView

--- screens/Dishes/DishProfileScreen/index.tsx
+++ screens/Dishes/DishProfileScreen/index.tsx
@@ -16,7 +16,7 @@ const MockDish = {
   allergens: ['Walnuts', 'Peanuts', 'Gluten'],
   imageLink: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-202102-airfryerchickenparm-180-ls-1612561654.jpg?crop=1.00xw:1.00xh;0,0&resize=980:*',
   favorite: true,
-  comments: 'Entered additional comments woot',
+  comments: '',
 } as Dish;
 
 export default function DishProfileScreen() {
@@ -40,83 +40,87 @@ export default function DishProfileScreen() {
   const onMinus = () => setQuantity((prevQuantity) => (prevQuantity === 0 ? prevQuantity : prevQuantity - 1));
 
   return (
-    <ScrollView style={{ backgroundColor: 'white' }}>
-      <View style={styles.container}>
-        <View style={{ paddingLeft: '5%', paddingTop: '5%' }}>
-          <ChevronButton
-            text="Back"
+    <ScrollView
+      contentContainerStyle={{
+        flexGrow: 1,
+      }}
+      style={styles.container}
+    >
+      <View style={{ flex: 2 }} />
+      <View style={{ flex: 2, paddingLeft: '5%', paddingTop: '5%' }}>
+        <ChevronButton
+          text="Back"
+          onPress={onPlus}
+        />
+      </View>
+      <View style={styles.imgContainer}>
+        <ImageBackground
+          source={{ uri: MockDish.imageLink }}
+          imageStyle={{
+            borderRadius: 4,
+            alignSelf: 'center',
+          }}
+          style={{
+            height: '100%',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <Icon.Button
+            name={favorite ? 'star' : 'star-o'}
+            size={30}
+            color="#F37B36"
+            backgroundColor="transparent"
+            onPress={addFavorite}
+            style={{
+              justifyContent: 'flex-end',
+            }}
+          />
+        </ImageBackground>
+      </View>
+      <Text style={styles.title}>{MockDish.dishName}</Text>
+      <View style={styles.detailContainer}>
+        <View style={{ flexDirection: 'row' }}>
+          <View>
+            <Text style={styles.detailLabel}>Price per Serving</Text>
+            <Text style={styles.detailValue}>$ {MockDish.cost.toString()}</Text>
+          </View>
+          <View>
+            <Text style={styles.detailLabel}>Weight per Serving</Text>
+            <Text style={styles.detailValue}>{MockDish.pounds} lbs</Text>
+          </View>
+        </View>
+        <View>
+          <Text style={styles.detailLabel} key={MockDish._id}>Allergen(s)</Text>
+          <Text style={styles.detailValue}>{allergens}</Text>
+        </View>
+        <View>
+          <Text style={styles.detailLabel} key={MockDish._id}>{MockDish.comments === '' ? '' : 'Additional comment(s)'}</Text>
+          <Text style={styles.detailValue}>{MockDish.comments}</Text>
+        </View>
+      </View>
+      <View style={styles.quantityContainer}>
+        <Text style={styles.quantityLabel}>Quantity</Text>
+        <View style={styles.quantityController}>
+          <Icon.Button
+            name="minus"
+            size={20}
+            color="#5D5D5D"
+            backgroundColor="transparent"
+            onPress={onMinus}
+          />
+          <Text style={styles.quantityCount}>{quantity}</Text>
+          <Icon.Button
+            name="plus"
+            size={20}
+            color="#5D5D5D"
+            backgroundColor="transparent"
             onPress={onPlus}
           />
         </View>
-        <View style={styles.imgContainer}>
-          <ImageBackground
-            source={{ uri: MockDish.imageLink }}
-            imageStyle={{
-              borderRadius: 4,
-              alignSelf: 'center',
-            }}
-            style={{
-              height: '100%',
-              justifyContent: 'flex-end',
-            }}
-          >
-            <Icon.Button
-              name={favorite ? 'star' : 'star-o'}
-              size={30}
-              color="#F37B36"
-              backgroundColor="transparent"
-              onPress={addFavorite}
-              style={{
-                justifyContent: 'flex-end',
-              }}
-            />
-          </ImageBackground>
-        </View>
-        <View style={styles.detailContainer}>
-          <Text style={styles.title}>{MockDish.dishName}</Text>
-          <View style={{ flexDirection: 'row' }}>
-            <View>
-              <Text style={styles.detailLabel}>Price per Serving</Text>
-              <Text style={styles.detailValue}>$ {MockDish.cost.toString()}</Text>
-            </View>
-            <View>
-              <Text style={styles.detailLabel}>Weight per Serving</Text>
-              <Text style={styles.detailValue}>{MockDish.pounds} lbs</Text>
-            </View>
-          </View>
-          {/* <View style={{ flex: }}>
-          <Text style={styles.detailLabel} key={MockDish._id}>Allergen(s)</Text>
-          <Text style={styles.detailValue}>{allergens}</Text>
-        </View> */}
-          <View>
-            <Text style={styles.detailLabel} key={MockDish._id}>{MockDish.comments === '' ? '' : 'Additional comment(s)'}</Text>
-            <Text style={styles.detailValue}>{MockDish.comments}</Text>
-          </View>
-        </View>
-        <View style={styles.quantityContainer}>
-          <Text style={styles.quantityLabel}>Quantity</Text>
-          <View style={styles.quantityController}>
-            <Icon.Button
-              name="minus"
-              size={20}
-              color="#5D5D5D"
-              backgroundColor="transparent"
-              onPress={onMinus}
-            />
-            <Text style={styles.quantityCount}>{quantity}</Text>
-            <Icon.Button
-              name="plus"
-              size={20}
-              color="#5D5D5D"
-              backgroundColor="transparent"
-              onPress={onPlus}
-            />
-          </View>
-        </View>
-        <TouchableOpacity style={styles.addToListButton}>
-          <Text style={styles.addToListText}>Add to donation list ({quantity})</Text>
-        </TouchableOpacity>
       </View>
+      <TouchableOpacity style={styles.addToListButton}>
+        <Text style={styles.addToListText}>Add to donation list ({quantity})</Text>
+      </TouchableOpacity>
     </ScrollView>
   );
 }
@@ -124,23 +128,26 @@ export default function DishProfileScreen() {
 // React Native Styles
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'white',
     paddingHorizontal: moderateScale(24),
     paddingVertical: moderateScale(25)
   },
   imgContainer: {
-    height: moderateScale(200),
+    flex: 2000,
+    marginVertical: moderateScale(5)
   },
   detailContainer: {
+    flexGrow: 5,
     paddingTop: moderateScale(12),
     flexDirection: 'column',
     flexWrap: 'wrap',
     width: '75%',
   },
   title: {
+    flex: 5,
     fontSize: moderateScale(25),
     fontWeight: '500',
     alignSelf: 'flex-start',
-    marginBottom: moderateScale(16)
   },
   detailLabel: {
     marginBottom: 8,
@@ -154,6 +161,7 @@ const styles = StyleSheet.create({
     fontSize: moderateScale(15)
   },
   quantityContainer: {
+    flex: 6,
     marginTop: moderateScale(5),
     marginBottom: moderateScale(20)
   },
@@ -176,6 +184,7 @@ const styles = StyleSheet.create({
     paddingTop: 8
   },
   addToListButton: {
+    flex: 4,
     backgroundColor: '#F37B36',
     alignItems: 'center',
     paddingVertical: moderateScale(12),
