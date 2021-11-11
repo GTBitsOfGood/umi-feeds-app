88b58f6519f3ee9a899c51bca28eee862817f89c
Ivan Zou <izou3@gatech.edu>
2021-11-10 21:39:07 -0500

    Attached DonationList to AuthState

--- components/DonationsList/index.tsx
+++ components/DonationsList/index.tsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ScrollView, View, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { useSelector } from 'react-redux';
 import { Text } from '../../style/Themed';
-import { DonationForm, DonationDishes, Address } from '../../types';
+import { DonationForm, DonationDishes, Address, Dish } from '../../types';
 import { DonationsListScreenParamList } from '../../navigation/AdminStack/DonationList/types';
 import { HomeScreenParamList } from '../../navigation/SharedStack/Home/types';
 import { orangeColor } from '../Button/styles';
 import { RootState } from '../../redux/rootReducer';
 import { ChevronButton } from '..';
+import { moderateScale } from '../../util/index';
 import styles from './styles';
 
 type DonationListBoxProps = StackNavigationProp<DonationsListScreenParamList, 'DonationsListScreen'>
@@ -97,34 +98,18 @@ const df2 = {
 const forms = [df1, df2];
 
 export default function DonationsList() {
+  const authState = useSelector((state: RootState) => state.auth);
+  const navigation = useNavigation<HomeScreenProps>();
+
   const [selectedList, setSelectedList] = useState('');
+  const [ongoingDonations, setOngoingDonations] = useState<JSX.Element[]>([]);
+  const [pastDonations, setPastDonations] = useState<JSX.Element[]>([]);
+
   const setId = (id?: string) => {
     if (id) {
       setSelectedList(id);
     }
   };
-
-  const ongoingDonations: JSX.Element[] = [];
-  const pastDonations: JSX.Element[] = [];
-  forms.forEach((donation) => {
-    if (donation.ongoing) {
-      ongoingDonations.push(<DonationListBox
-        key={donation._id}
-        donation={donation}
-        selectedId={selectedList}
-        setSelectedList={setId}
-      />);
-    } else {
-      pastDonations.push(<DonationListBox
-        key={donation._id}
-        donation={donation}
-        selectedId={selectedList}
-        setSelectedList={setId}
-      />);
-    }
-  });
-
-  const navigation = useNavigation<HomeScreenProps>();
 
   return (
     <View style={styles.container}>
@@ -134,40 +119,71 @@ export default function DonationsList() {
         <ChevronButton text="Back" onPress={() => navigation.navigate('Home')} />
         <Text style={styles.title}>My donations</Text>
         <Text style={styles.subtitle}>Ongoing Donations</Text>
-        {ongoingDonations}
+        {
+          authState.donations.map((donation) => {
+            if (donation.ongoing) {
+              return (
+                <DonationListBox
+                  key={donation._id}
+                  donation={donation}
+                  selectedId={selectedList}
+                  setSelectedList={setId}
+                  dishes={authState.dishes}
+                />
+              );
+            } else {
+              return null;
+            }
+          })
+        }
         <Text style={styles.subtitle}>Past Donations</Text>
-        {pastDonations}
+        {
+          authState.donations.map((donation) => {
+            if (!donation.ongoing) {
+              return (
+                <DonationListBox
+                  key={donation._id}
+                  donation={donation}
+                  selectedId={selectedList}
+                  setSelectedList={setId}
+                  dishes={authState.dishes}
+                />
+              );
+            } else {
+              return null;
+            }
+          })
+        }
       </ScrollView>
     </View>
   );
 }
 
-function DonationListBox(props: { donation: DonationForm, selectedId: string, setSelectedList: (param1?: string) => void }) {
+function DonationListBox(props: { donation: DonationForm, selectedId: string, dishes: Dish[], setSelectedList: (param1?: string) => void }) {
   const navigation = useNavigation<DonationListBoxProps>();
-
+  const { dishes, donation } = props;
   const endTime = props.donation.pickupEndTime.toLocaleString().slice(0, 9);
   let pickupTime = 'TBA';
   let color = '#FC8834';
   if (props.donation.pickupEndTime !== undefined) {
-    pickupTime = props.donation.pickupEndTime.toLocaleString().slice(11);
+    pickupTime = new Date(props.donation.pickupEndTime).toLocaleDateString('en-US');
     color = '#3E3E3E';
   }
 
-  const authState = useSelector((state: RootState) => state.auth);
-  const donationDish = [];
-  for (let i = 0; i < props.donation.donationDishes.length; i += 1) {
-    let dishName = props.donation.donationDishes[i].dishID;
-    for (let j = 0; j < authState.dishes.length; j += 1) {
-      if (authState.dishes[j]._id === props.donation.donationDishes[i].dishID) {
-        dishName = authState.dishes[i].dishName;
-      }
-    }
-    if (i === props.donation.donationDishes.length - 1) {
-      donationDish.push(<Text style={{ color, fontSize: 14, flex: 1 }} key={props.donation.donationDishes[i]._id}>{dishName} ({props.donation.donationDishes[i].quantity})</Text>);
-    } else {
-      donationDish.push(<Text style={{ color, fontSize: 14, flex: 1 }} key={props.donation.donationDishes[i]._id}>{dishName} ({props.donation.donationDishes[i].quantity}), </Text>);
-    }
-  }
+  // const donationDish = [];
+  // if (donation.dishes) {
+  //   for (let i = 0; i < donation.dishes.length; i += 1) {
+  //     const { dishID } = donation.dishes[i];
+  //     const numDish = dishes.length < 5 ? dishes.length : 5;
+  //     for (let j = 0; j < numDish; j += 1) {
+  //       console.log(dishes[j]._id);
+  //       if (dishes[j]._id === dishID) {
+  //         donationDish.push(dishes[j].dishName);
+  //         // donationDish.push(<Text style={{ color, fontSize: 14, flex: 1 }} key={props.donation.donationDishes[i]._id}>{dishes[j].dishName} ({props.donation.donationDishes[i].quantity})</Text>);
+  //       }
+  //     }
+  //   }
+  // }
 
   return (
     <Pressable
@@ -178,14 +194,16 @@ function DonationListBox(props: { donation: DonationForm, selectedId: string, se
         borderColor: orangeColor,
         borderWidth: 2,
         backgroundColor: 'rgba(243, 123, 54, 0.15)',
-        height: 161
+        height: moderateScale(120),
+        borderRadius: 4
       } : {
         borderWidth: 2,
+        borderRadius: 4,
         marginBottom: 10,
         paddingVertical: 16,
         paddingHorizontal: 18,
         borderColor: color,
-        height: 161 }}
+        height: moderateScale(120) }}
       onPress={() => props.setSelectedList(props.donation._id)}
     >
       <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
@@ -197,8 +215,15 @@ function DonationListBox(props: { donation: DonationForm, selectedId: string, se
           View ⟩
         </Text>
       </View>
-      <View style={{ flex: 1 }}>
-        <Text>{donationDish} </Text>
+      {/* <View style={{ flex: 3, paddingVertical: 10 }}>
+        <Text>
+          {
+            donationDish.map((dish) => <Text style={{ color, fontSize: 14, flex: 1 }} key={dish}>{dish}, </Text>)
+          }
+        </Text>
+      </View> */}
+      <View style={{ flex: 2, marginVertical: 5 }}>
+        <Text style={{ color, fontSize: 15 }}>{donation.pickupInstructions}</Text>
       </View>
       <View style={{ flex: 1 }}>
         <Text style={{ color, fontSize: 15 }}>{`Picked up at: ${pickupTime}`}</Text>
