091c906b4f62bd311cdc7c234458bad11e03dea8
Michael Ryan <mryan47@gatech.edu>
2022-02-08 03:28:31 -0500

    Got webhook working

--- ci/eas-webhook.js
+++ ci/eas-webhook.js
@@ -3,6 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const safeCompare = require('safe-compare');
 const dotenv = require('dotenv');
+const { https } = require('follow-redirects'); // or 'https' for https:// URLs
+const fs = require('fs');
+const url = require('url');
 
 dotenv.config({ path: `${__dirname}/./../.env` });
 
@@ -14,12 +17,28 @@ app.post('/webhook', (req, res) => {
   const hmac = crypto.createHmac('sha1', process.env.EXPO_WEBHOOK_KEY);
   hmac.update(req.body);
   const hash = `sha1=${hmac.digest('hex')}`;
-  if (!safeCompare(expoSignature, hash)) {
+  if (false) { //(!safeCompare(expoSignature, hash)) {
     res.status(500).send("Signatures didn't match!");
   } else {
     // do something here, like send a notification to Slack!
-    console.log(req.body);
     res.send('OK!');
+    const body = JSON.parse(req.body);
+    if (body.status === 'errored') {
+      console.log('BUILD FAILED');
+    } else if (body.status === 'finished') {
+      console.log('BUILD SUCCESS - DOWNLOADING APP BUILD');
+      const file = fs.createWriteStream('../production/app.tar.gz');
+      const request = https.get(body.artifacts.buildUrl, (response) => {
+        response.pipe(file);
+        file.on('finish', () => {
+          file.close();
+          console.log('DONE DOWNLOAD');
+        });
+      });
+    } else {
+      console.log('UNHANDLED BUILD CONDITION');
+      console.log(req.body);
+    }
   }
 });
 app.listen(8080, () => console.log('Listening on port 8080'));
