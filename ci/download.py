2e9bf273c2d587dab7c694508615d7224a82b2d8
Michael Ryan <mryan47@gatech.edu>
2022-02-08 05:05:30 -0500

    Switched action to staging branch

--- ci/download.py
+++ ci/download.py
@@ -1,9 +1,13 @@
 import sys
 import requests
+import os
  
 file_url = sys.argv[1]
  
 file_stream = requests.get(file_url, stream=True)
+
+if(not os.path.exists('../production')):
+    os.makedirs('../production')
  
 with open('../production/app.tar.gz', 'wb') as local_file:
     for data in file_stream:
